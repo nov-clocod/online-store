3b51f6a2b47b6475c8c0a12c5fa1600b935397dc
Sthealth <AW@example.com>
2025-10-14 11:36:21 -0400

    create product class gettors and tostring

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class Product {
+    private String sku;
+    private String productName;
+    private double price;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s|%s|%.2f", this.sku, this.productName, this.price);
+    }
 }
